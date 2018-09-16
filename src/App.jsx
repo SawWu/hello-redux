9c4b746603cf821bc3020f974ab2fa5356d48394
Liu Ren <liuren@cc1st.com>
2018-09-16 18:20:11 +0800

    redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
 import React, {Component} from 'react';
-import {createStore} from 'redux'
+import {Container, Row, Col, Button} from 'reactstrap';
 import PropTypes from 'prop-types';
 
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
+      <div className="app" style={{margin: '100px auto 0'}}>
+        <Container>
+          <h1 className="jumbotron-heading text-center" style={{margin: '0 auto 30px'}}>{this.props.value}</h1>
+          <p className="text-center">
+            <Button color="primary" style={{marginRight: '30px'}} onClick={this.props.onIncrement}>INCREMENT</Button>
+            <Button color="danger" style={{marginLeft: '30px'}} onClick={this.props.onDecrement}>DECREMENT</Button>
+          </p>
+        </Container>
       </div>
     );
   }
+}
+
+App.propTypes = {
+  value: PropTypes.number.isRequired,
+  onIncrement: PropTypes.func.isRequired,
+  onDecrement: PropTypes.func.isRequired
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import {render} from 'react-dom';
+import ReactDOM from 'react-dom';
 import App from './App';
 import {createStore} from 'redux';
 import reducer from './reducers/counter';
 
 let store = createStore(reducer);
-const init = store.getState();
-console.log(init);
 
-render(
-  <App/>,
-  document.querySelector('#root')
-);
+store.subscribe(() => console.log("State updated!", store.getState()));
+
+const render=()=>{
+  ReactDOM.render(
+    <App value={store.getState()}
+         onIncrement={() => store.dispatch({type: "INCREMENT"})}
+         onDecrement={() => store.dispatch({type: "DECREMENT"})}/>,
+    document.querySelector('#root')
+  );
+}
+
+render();
+store.subscribe(render);
+
+
 
