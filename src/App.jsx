68fc12fbbfe8f909d3f23b0487de517b526761d3
Liu Ren <liuren@cc1st.com>
2018-09-16 15:58:24 +0800

    初始化项目

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        {'322323'}
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(< App / >, document.getElementById('root')
-)
-;
-registerServiceWorker();
+render(
+  <App />,
+  document.querySelector('#root')
+);
+
