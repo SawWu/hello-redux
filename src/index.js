1faf658a6349516e3ffdb4997498ca4da6a0ebd6
Liu Ren <liuren@cc1st.com>
2018-10-05 22:44:07 +0800

    开发调式工具

--- src/index.js
+++ src/index.js
@@ -7,9 +7,10 @@ import {Provider} from 'react-redux';
 import rootReducer from './reducers';
 import thunk from 'redux-thunk';
 import promise from 'redux-promise-middleware'
+import {composeWithDevTools} from 'redux-devtools-extension';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const store = createStore(rootReducer, {}, applyMiddleware(thunk,promise()));
+const store = createStore(rootReducer, {}, composeWithDevTools(applyMiddleware(thunk, promise())));
 
 ReactDOM.render(
   <Provider store={store}>
