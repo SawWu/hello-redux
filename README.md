3867ded1a9f365727341e37fb6c550ba4e66c529
Ren Liu <liuren@cc1st.com>
2019-03-10 20:05:57 +0800

    初始化项目

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# 
+# hello-redux
 
 redux-learn
--- src/reducers/counter.js
+++ src/reducers/counter.js
@@ -1,11 +1,10 @@
-const counter = (state = 1, action = {}) => {
-  switch (action.type) {
+const counter = (state = 0, action = {}) => {
+  switch(action.type) {
     case 'INCREMENT':
-      return state < 1 ? state : state + 1;
+      return state + 1;
     case 'DECREMENT':
-      return state > 1 ? state - 1 : state;
-    default:
-      return state;
+      return state - 1;
+    default: return state;
   }
 }
 
